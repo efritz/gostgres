f5dcae3e88b61c38c981c386cc3ffa7635526ef9
Eric Fritz <eric@eric-fritz.com>
2024-04-27 22:35:03 -0500

    Update promotion logic.

--- internal/shared/promotion.go
+++ internal/shared/promotion.go
@@ -6,22 +6,26 @@ import (
 )
 
 func PromoteToCommonNumericValues(left, right any) (any, any, error) {
-	lIndex := numericTypeIndex(left)
-	if lIndex < 0 {
-		return nil, nil, fmt.Errorf("unexpected type (wanted numeric type, have %v)", left)
-	}
+	for {
+		lIndex := numericTypeIndex(left)
+		if lIndex < 0 {
+			return nil, nil, fmt.Errorf("unexpected type (wanted numeric type, have %v)", left)
+		}
 
-	rIndex := numericTypeIndex(right)
-	if rIndex < 0 {
-		return nil, nil, fmt.Errorf("unexpected type (wanted numeric type, have %v)", right)
-	}
+		rIndex := numericTypeIndex(right)
+		if rIndex < 0 {
+			return nil, nil, fmt.Errorf("unexpected type (wanted numeric type, have %v)", right)
+		}
 
-	if lIndex < rIndex {
-		return promoters[lIndex][rIndex](left), right, nil
-	}
+		if lIndex == rIndex {
+			break
+		}
 
-	if rIndex < lIndex {
-		return left, promoters[rIndex][lIndex](right), nil
+		if lIndex < rIndex {
+			left = promoters[lIndex][rIndex](left)
+		} else if rIndex < lIndex {
+			right = promoters[rIndex][lIndex](right)
+		}
 	}
 
 	return left, right, nil
@@ -29,52 +33,58 @@ func PromoteToCommonNumericValues(left, right any) (any, any, error) {
 
 var promoters = [][]func(value any) any{
 	{
-		func(value any) any { panic("impossible promotion") },               // smallint -> smallint
-		func(value any) any { return int32(value.(int16)) },                 // smallint -> integer
-		func(value any) any { return int64(value.(int16)) },                 // smallint -> bigint
-		func(value any) any { return float32(value.(int16)) },               // smallint -> real
-		func(value any) any { return float64(value.(int16)) },               // smallint -> double precision
-		func(value any) any { return big.NewFloat(float64(value.(int16))) }, // smallint -> numeric
+		// smallint ->
+		func(value any) any { panic("impossible promotion") },               //  -> smallint
+		func(value any) any { return int32(value.(int16)) },                 //  -> integer
+		func(value any) any { return int64(value.(int16)) },                 //  -> bigint
+		func(value any) any { return float32(value.(int16)) },               //  -> real
+		func(value any) any { return float64(value.(int16)) },               //  -> double precision
+		func(value any) any { return big.NewFloat(float64(value.(int16))) }, //  -> numeric
 	},
 	{
-		func(value any) any { panic("impossible promotion") },               // integer -> smallint
-		func(value any) any { panic("impossible promotion") },               // integer -> integer
-		func(value any) any { return int64(value.(int32)) },                 // integer -> bigint
-		func(value any) any { return float32(value.(int32)) },               // integer -> real
-		func(value any) any { return float64(value.(int32)) },               // integer -> double precision
-		func(value any) any { return big.NewFloat(float64(value.(int32))) }, // integer -> numeric
+		// integer ->
+		func(value any) any { panic("impossible promotion") },               // -> smallint
+		func(value any) any { panic("impossible promotion") },               // -> integer
+		func(value any) any { return int64(value.(int32)) },                 // -> bigint
+		func(value any) any { return float32(value.(int32)) },               // -> real
+		func(value any) any { return float64(value.(int32)) },               // -> double precision
+		func(value any) any { return big.NewFloat(float64(value.(int32))) }, // -> numeric
 	},
 	{
-		func(value any) any { panic("impossible promotion") },               // bigint -> smallint
-		func(value any) any { panic("impossible promotion") },               // bigint -> integer
-		func(value any) any { panic("impossible promotion") },               // bigint -> bigint
-		func(value any) any { return float32(value.(int64)) },               // bigint -> real (as double precision)
-		func(value any) any { return float64(value.(int64)) },               // bigint -> double precision
-		func(value any) any { return big.NewFloat(float64(value.(int64))) }, // bigint -> numeric
+		// bigint ->
+		func(value any) any { panic("impossible promotion") },               // -> smallint
+		func(value any) any { panic("impossible promotion") },               // -> integer
+		func(value any) any { panic("impossible promotion") },               // -> bigint
+		func(value any) any { return float64(value.(int64)) },               // -> real (NOTE: skips to double precision)
+		func(value any) any { return float64(value.(int64)) },               // -> double precision
+		func(value any) any { return big.NewFloat(float64(value.(int64))) }, // -> numeric
 	},
 	{
-		func(value any) any { panic("impossible promotion") },                 // real -> smallint
-		func(value any) any { panic("impossible promotion") },                 // real -> integer
-		func(value any) any { panic("impossible promotion") },                 // real -> bigint
-		func(value any) any { panic("impossible promotion") },                 // real -> real
-		func(value any) any { return float64(value.(float32)) },               // real -> double precision
-		func(value any) any { return big.NewFloat(float64(value.(float32))) }, // real -> numeric
+		// real ->
+		func(value any) any { panic("impossible promotion") },                 // -> smallint
+		func(value any) any { panic("impossible promotion") },                 // -> integer
+		func(value any) any { panic("impossible promotion") },                 // -> bigint
+		func(value any) any { panic("impossible promotion") },                 // -> real
+		func(value any) any { return float64(value.(float32)) },               // -> double precision
+		func(value any) any { return big.NewFloat(float64(value.(float32))) }, // -> numeric
 	},
 	{
-		func(value any) any { panic("impossible promotion") },                 // double precision -> smallint
-		func(value any) any { panic("impossible promotion") },                 // double precision -> integer
-		func(value any) any { panic("impossible promotion") },                 // double precision -> bigint
-		func(value any) any { panic("impossible promotion") },                 // double precision -> real
-		func(value any) any { panic("impossible promotion") },                 // double precision -> double precision
-		func(value any) any { return big.NewFloat(float64(value.(float64))) }, // double precision -> numeric
+		// double precision ->
+		func(value any) any { panic("impossible promotion") },                 // -> smallint
+		func(value any) any { panic("impossible promotion") },                 // -> integer
+		func(value any) any { panic("impossible promotion") },                 // -> bigint
+		func(value any) any { panic("impossible promotion") },                 // -> real
+		func(value any) any { panic("impossible promotion") },                 // -> double precision
+		func(value any) any { return big.NewFloat(float64(value.(float64))) }, // -> numeric
 	},
 	{
-		func(value any) any { panic("impossible promotion") }, // numeric -> smallint
-		func(value any) any { panic("impossible promotion") }, // smallint -> integer
-		func(value any) any { panic("impossible promotion") }, // smallint -> bigint
-		func(value any) any { panic("impossible promotion") }, // numeric -> real
-		func(value any) any { panic("impossible promotion") }, // numeric -> double precision
-		func(value any) any { panic("impossible promotion") }, // numeric -> numeric
+		// numeric ->
+		func(value any) any { panic("impossible promotion") }, // -> smallint
+		func(value any) any { panic("impossible promotion") }, // -> integer
+		func(value any) any { panic("impossible promotion") }, // -> bigint
+		func(value any) any { panic("impossible promotion") }, // -> real
+		func(value any) any { panic("impossible promotion") }, // -> double precision
+		func(value any) any { panic("impossible promotion") }, // -> numeric
 	},
 }
 

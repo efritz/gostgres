b21288837a9088235523dc43e01129a6d0383e87
Eric Fritz <eric@eric-fritz.com>
2024-05-23 21:23:27 -0500

    Remove unnecessary interface function.

--- internal/functions/context.go
+++ internal/functions/context.go
@@ -3,6 +3,5 @@ package functions
 import "github.com/efritz/gostgres/internal/sequence"
 
 type FunctionContext interface {
-	GetFunction(name string) (Function, bool)
 	GetSequence(name string) (*sequence.Sequence, bool)
 }

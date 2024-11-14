d55a7e594cafa728edc18bbbf159290ea0b0d74f
Eric Fritz <eric@eric-fritz.com>
2024-11-13 22:35:20 -0600

    Add basic helper to resolve expressions.

---
+++ internal/syntax/ast/expressions.go
@@ -0,0 +1,30 @@
+package ast
+
+import (
+	"github.com/efritz/gostgres/internal/execution/expressions"
+	"github.com/efritz/gostgres/internal/shared/impls"
+)
+
+func ResolveExpression(ctx *impls.NodeResolutionContext, expr impls.Expression) (impls.Expression, error) {
+	mappedExpr, err := expr.Map(func(expr impls.Expression) (impls.Expression, error) {
+		if named, ok := expr.(expressions.NamedExpression); ok {
+			field, err := ctx.Lookup(named.Field().RelationName(), named.Field().Name())
+			if err != nil {
+				return nil, err
+			}
+
+			return expressions.NewNamed(field), nil
+		}
+
+		return expr, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if err := expr.Resolve(ctx.ExpressionResolutionContext()); err != nil {
+		return nil, err
+	}
+
+	return mappedExpr, nil
+}

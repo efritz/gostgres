736bdba99cabe2b5997d15b44adf4bd8099ac9b2
Eric Fritz <eric@eric-fritz.com>
2024-11-19 17:11:22 -0600

    Update projection string function.

--- internal/execution/projection/projection.go
+++ internal/execution/projection/projection.go
@@ -1,6 +1,7 @@
 package projection
 
 import (
+	"fmt"
 	"slices"
 	"strings"
 
@@ -51,12 +52,32 @@ func NewProjectionFromProjectedExpressions(
 
 func (p *Projection) String() string {
 	fields := make([]string, 0, len(p.aliases))
+
+	relationNames := map[string]struct{}{}
 	for _, expression := range p.aliases {
-		if named, ok := expression.Expression.(expressions.NamedExpression); ok && named.Field().Name() == expression.Alias {
-			fields = append(fields, expression.Alias)
-		} else {
-			fields = append(fields, expression.String())
+		if named, ok := expression.Expression.(expressions.NamedExpression); ok {
+			relationNames[named.Field().RelationName()] = struct{}{}
 		}
+	}
+
+	for _, expression := range p.aliases {
+		// TODO - simplify named expressions below top-level?
+		if named, ok := expression.Expression.(expressions.NamedExpression); ok {
+			name := named.Field().String()
+			if len(relationNames) == 1 {
+				name = named.Field().Name()
+			}
+
+			if named.Field().Name() == expression.Alias {
+				fields = append(fields, name)
+			} else {
+				fields = append(fields, fmt.Sprintf("%s as %s", name, expression.Alias))
+			}
+
+			continue
+		}
+
+		fields = append(fields, expression.String())
 	}
 
 	return strings.Join(fields, ", ")

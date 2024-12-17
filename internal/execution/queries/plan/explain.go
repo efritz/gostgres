12c7299202d2b5680dab255a46fd0102a613ec34
Eric Fritz <eric@eric-fritz.com>
2024-12-17 09:56:07 -0600

    Make explain node consistent.

--- internal/execution/queries/plan/explain.go
+++ internal/execution/queries/plan/explain.go
@@ -11,22 +11,16 @@ type logicalExplain struct {
 	LogicalNode
 }
 
-func NewExplain(n LogicalNode) *logicalExplain {
+func NewExplain(n LogicalNode) LogicalNode {
 	return &logicalExplain{
 		LogicalNode: n,
 	}
 }
 
-func (n *logicalExplain) Name() string {
-	return "EXPLAIN"
-}
-
 var queryPlanField = fields.NewField("", "query plan", types.TypeText, fields.NonInternalField)
 
-func (n *logicalExplain) Fields() []fields.Field {
-	return []fields.Field{queryPlanField}
-}
-
+func (n *logicalExplain) Name() string                                                        { return "EXPLAIN" }
+func (n *logicalExplain) Fields() []fields.Field                                              { return []fields.Field{queryPlanField} }
 func (n *logicalExplain) AddFilter(ctx impls.OptimizationContext, filter impls.Expression)    {} // top-level
 func (n *logicalExplain) AddOrder(ctx impls.OptimizationContext, order impls.OrderExpression) {} // top-level
 func (n *logicalExplain) Filter() impls.Expression                                            { return nil }
